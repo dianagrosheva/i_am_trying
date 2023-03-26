3e8da19f450247b75c4c850b3edfa0acfea1342b
Diana Grosheva <leslielsonn@gmail.com>
2023-03-26 16:50:38 +0300

    triangle_type

---
+++ lesslon 1/triangle_type.py
@@ -0,0 +1,15 @@
+def triangle_type ():
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    if a + b > c and a + c > b and b + c > a:
+        if a != b != c and a != c != b:
+            print('Прямоугольный треугольник')
+        elif a == b == c:
+            print('Равносторонний треугольник')
+        else:
+            print('Равнобедренный треугольник')
+    else:
+        print('Ты долбаеб')
+
+triangle_type()
