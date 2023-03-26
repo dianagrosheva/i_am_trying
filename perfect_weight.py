4bd796b0dbc2e41f6e0077022777d59c673a1309
Diana Grosheva <99553621+dianagrosheva@users.noreply.github.com>
2023-03-26 13:39:27 +0300

    Delete perfect_weight.py

--- perfect_weight.py
+++
@@ -1,8 +0,0 @@
-def get_perfect_weight (name, height):
-    perfect_weight = (height-110)*1.15
-    if perfect_weight < 0:
-        print (name + ', ваш вес уже оптимальный')
-    else:
-        print (int(perfect_weight))
-
-get_perfect_weight('Диана', 95)
