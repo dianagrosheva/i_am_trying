77b65638097c3040e29c7f384b625aba97279afb
Diana Grosheva <leslielsonn@gmail.com>
2023-03-26 23:31:47 +0300

    changes

--- lesslon 1/perfect_weight.py
+++ lesslon 1/perfect_weight.py
@@ -1,8 +1,15 @@
-def get_perfect_weight (name, height):
-    perfect_weight = (height-110)*1.15
+def get_perfect_weight ():
+
+    print ('Введите ваше имя')
+    name = input()
+    
+    print ('Введите ваш рост')
+    heigth = input()
+
+    perfect_weight = (int(heigth)-110)*1.15
     if perfect_weight < 0:
         print (name + ', ваш вес уже оптимальный')
     else:
-        print (int(perfect_weight))
+        print (name + ', ваш идеальный вес составляет ' + str(int(perfect_weight)) + ' кг')
 
-get_perfect_weight('Диана', 95)
+get_perfect_weight()
